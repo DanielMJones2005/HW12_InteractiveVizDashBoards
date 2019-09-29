cfb9b6ba8a40316f5d6addd9def715b22ac42f07
Bivittatus <Python.Bivittatus.8518@gmail.com>
2019-09-29 00:21:34 -0500

    9/29/2019 complete HW

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -21,10 +21,106 @@ function buildMetadata(sample) {
   
   });
     
-
-       
-    // BONUS: Build the Gauge Chart
+// BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+  d3.json(metaUrl).then((sample) => {
+      let washData = sample.WFREQ;
+      console.log(washData)
+  // Wash Frequency | Level
+      let level = washData     
+  
+  // Trig to calc meter point
+      var degrees = 180 - parseInt(level)*20,
+         radius = .5;
+      var radians = degrees * Math.PI / 180;
+      var x = radius * Math.cos(radians);
+      var y = radius * Math.sin(radians);
+  
+  // Path: may have to change to create a better triangle
+     var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+      var path = mainPath.concat(pathX,space,pathY,pathEnd);
+  
+      var data = [{ type: 'scatter',
+        x: [0], y:[0],
+        marker: {size: 28, color:'850000'},
+        showlegend: false,
+        name: 'Washes',
+        text: level,
+        hoverinfo: 'text+name'},
+        { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+        rotation: 90,
+        text: [
+          '8-9',
+          '7-8', 
+          '6-7', 
+          '5-6', 
+          '4-5', 
+          '3-4', 
+          '2-3',
+          '1-2', 
+          '0-1', 
+          '',
+          ],
+        textinfo: 'text',
+        textposition:'inside',	  
+        marker: {colors:[
+              'rgba(5, 38, 0, .5)', // 8-9
+              'rgba(11, 92, 0, .5)', // 7-8
+              'rgba(14, 127, 0, .5)', // 6-7
+              'rgba(110, 154, 22, .5)', // 5-6 
+              'rgba(170, 202, 42, .5)', // 4-5  
+              'rgba(202, 209, 95, .5)', // 3-4
+              'rgba(210, 206, 145, .5)', // 2-3
+              'rgba(229, 228, 227, .5)', // 1-2
+              'rgba(239, 238, 237, .5)', // 0-1
+              'rgba(255, 255, 255, 0)', // bottom
+              '',
+              ]},
+        labels: [
+          '8-9', 
+          '7-8', 
+          '6-7', 
+          '5-6', 
+          '4-5', 
+          '3-4', 
+          '2-3', 
+          '1-2', 
+          '0-1', 
+          ''
+          ],
+        hoverinfo: 'label',
+       hole: .5,
+        type: 'pie',
+        showlegend: false
+      }];
+  
+      var layout = {
+        shapes:[{
+            type: 'path',
+            path: path,
+            fillcolor: '850000',
+            line: {
+              color: '850000'
+            }
+         }],
+       title: '<b>Belly Button Washing Frequency</b><br>Scrubs per Week',
+        //height: 1000,
+        //width: 1000,
+        xaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]},
+       yaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]}
+     };
+  
+    Plotly.newPlot('gauge', data, layout);
+  
+  });
+       
+  
 }
 
 function buildCharts(sample) {
@@ -40,25 +136,40 @@ function buildCharts(sample) {
     let markColor = sample.otu_ids;
     let textLabels = sample.otu_labels
 
-    var bubbleTrace = {
+    //var desired_maximum_marker_size = 40;
+
+    let bubbleTrace = {
       x: xValues,
       y: yValues,
       text: textLabels,
       mode: 'markers',
       marker: {
         color: markColor,
-        size: markSize
+        size: markSize,
+        //sizeref: Math.max(markSize) / (desired_maximum_marker_size),
+        //sizemode: 'area'
       }
     };
     
-    var bubbleLayout = {
-      title: 'Marker Size',
-      showlegend: false,
+    let bubbleData = [bubbleTrace];
+
+    let bubbleLayout = {
+      title: "<b>Sample Distributions and Sizes</b>",
+      xaxis: {
+        title: {
+          text: 'otu ids',
+        }},
+      yaxis: {
+        title: {
+          text: 'sample values',
+        }},
+
+      showlegend: false
       //height: 600,
       //width: 600
     };
     
-    Plotly.newPlot("bubble", bubbleTrace, bubbleLayout);
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
   });
   
 
@@ -73,22 +184,26 @@ function buildCharts(sample) {
       labels: sampleLabels,
       hovertext: sampleText,
       type: "pie",
-      title: "Top Samples"
+      title: "<b>Top Samples</b><br> "
+      
     }];
 
     let pieLayout = {
-      height: 600,
-      width: 800,
+      //height: 600,
+      //width: 100,
       showlegend: true
     };
 
     Plotly.newPlot("pie", pieTrace, pieLayout);
   });
+
+
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-
-
 }
+
+
+ 
 
 
 
